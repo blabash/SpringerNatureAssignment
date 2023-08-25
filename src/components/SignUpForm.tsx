2ac88c60afdef0211c39521a225f8046e47edf4b
Brett Labash <brett.labash@gmail.com>
2023-08-25 22:20:41 +0900

    Add desktop layout styling

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -31,138 +31,141 @@ export const SignUpForm = (props: Props) => {
   };
 
   return (
-    <main className="form-container">
-      <h1 className="form__title">Sign up form</h1>
-      <em className="form__description">All fields are required.</em>
-      <form className="inputs-container" onSubmit={handleSubmit}>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="first-name">
-            First name
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            name="firstName"
-            id="first-name"
-            value={userInfo.firstName}
-            onChange={handleUserInfoChange}
-            placeholder="ex: Jane"
-            required
-          />
-        </div>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="last-name">
-            Last name
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            name="lastName"
-            id="last-name"
-            value={userInfo.lastName}
-            onChange={handleUserInfoChange}
-            placeholder="ex: Doe"
-            required
-          />
-        </div>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="email">
-            Email address
-          </label>
-          <input
-            className="form__input"
-            type="email"
-            name="emailAddress"
-            id="email"
-            value={userInfo.emailAddress}
-            onChange={handleUserInfoChange}
-            placeholder="ex: jane.doe@gmail.com"
-            required
-          />
-        </div>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="password">
-            Password
-          </label>
-          <div>
+    <main className="form__container">
+      <div className="form__left-panel">
+        <h1 className="form__title">Sign up form</h1>
+        <em className="form__description">All fields are required.</em>
+        <form className="inputs-container" onSubmit={handleSubmit}>
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="first-name">
+              First name
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="firstName"
+              id="first-name"
+              value={userInfo.firstName}
+              onChange={handleUserInfoChange}
+              placeholder="ex: Jane"
+              required
+            />
+          </div>
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="last-name">
+              Last name
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="lastName"
+              id="last-name"
+              value={userInfo.lastName}
+              onChange={handleUserInfoChange}
+              placeholder="ex: Doe"
+              required
+            />
+          </div>
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="email">
+              Email address
+            </label>
+            <input
+              className="form__input"
+              type="email"
+              name="emailAddress"
+              id="email"
+              value={userInfo.emailAddress}
+              onChange={handleUserInfoChange}
+              placeholder="ex: jane.doe@gmail.com"
+              required
+            />
+          </div>
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
+            <div>
+              <div className="input-with-btn">
+                <input
+                  className="form__input"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  value={userInfo.password}
+                  onChange={handleUserInfoChange}
+                  title="Minimum 12 characters. Must contain at least 2 upper case letters, 2 numbers, and 2 symbols."
+                  pattern="^(?=(.*[A-Z]){2})(?=(.*\d){2})(?=(.*[\W_]){2}).{12,}$"
+                  required
+                />
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowPassword((s) => !s);
+                  }}
+                >
+                  {showPassword ? "Hide" : "Show"}
+                </button>
+              </div>
+              <ul className="form__password-reqs">
+                <li>Minimum 12 characters</li>
+                <li>
+                  Must contain at least 2 upper case letters, 2 numbers, and 2
+                  symbols
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="confirm-password">
+              Confirm password
+            </label>
             <div className="input-with-btn">
               <input
                 className="form__input"
-                type={showPassword ? "text" : "password"}
-                name="password"
-                id="password"
-                value={userInfo.password}
+                type={showConfirmPassword ? "text" : "password"}
+                name="confirmPassword"
+                id="confirm-password"
+                value={userInfo.confirmPassword}
                 onChange={handleUserInfoChange}
-                title="Minimum 12 characters. Must contain at least 2 upper case letters, 2 numbers, and 2 symbols."
-                pattern="^(?=(.*[A-Z]){2})(?=(.*\d){2})(?=(.*[\W_]){2}).{12,}$"
+                title="Must match password."
+                pattern={`^${userInfo.password}$`}
                 required
               />
               <button
                 onClick={(e) => {
                   e.preventDefault();
-                  setShowPassword((s) => !s);
+                  setShowConfirmPassword((s) => !s);
                 }}
               >
-                {showPassword ? "Hide" : "Show"}
+                {showConfirmPassword ? "Hide" : "Show"}
               </button>
             </div>
-            <ul className="form__password-reqs">
-              <li>Minimum 12 characters</li>
-              <li>
-                Must contain at least 2 upper case letters, 2 numbers, and 2
-                symbols
-              </li>
-            </ul>
           </div>
-        </div>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="confirm-password">
-            Confirm password
-          </label>
-          <div className="input-with-btn">
-            <input
-              className="form__input"
-              type={showConfirmPassword ? "text" : "password"}
-              name="confirmPassword"
-              id="confirm-password"
-              value={userInfo.confirmPassword}
-              onChange={handleUserInfoChange}
-              title="Must match password."
-              pattern={`^${userInfo.password}$`}
-              required
-            />
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setShowConfirmPassword((s) => !s);
-              }}
-            >
-              {showConfirmPassword ? "Hide" : "Show"}
-            </button>
-          </div>
-        </div>
-        <div className="form__input-row">
-          <label className="form__label" htmlFor="terms-conditions">
-            Terms and conditions
-          </label>
-          <div className="flex-row gap-half">
-            <input
-              type="checkbox"
-              name="termsAgreed"
-              id="terms-conditions"
-              checked={userInfo.termsAgreed}
-              onChange={handleUserInfoChange}
-              required
-            />
-            <label htmlFor="terms-conditions">
-              I agree to the terms and conditions
+          <div className="form__input-row">
+            <label className="form__label" htmlFor="terms-conditions">
+              Terms and conditions
             </label>
+            <div className="flex-row gap-half">
+              <input
+                type="checkbox"
+                name="termsAgreed"
+                id="terms-conditions"
+                checked={userInfo.termsAgreed}
+                onChange={handleUserInfoChange}
+                required
+              />
+              <label htmlFor="terms-conditions">
+                I agree to the terms and conditions
+              </label>
+            </div>
           </div>
-        </div>
-        <button className="form__submit-btn" type="submit">
-          Sign Up
-        </button>
-      </form>
+          <button className="form__submit-btn" type="submit">
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <div className="form__right-panel"></div>
     </main>
   );
 };
